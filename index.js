89bed325869e58ab75ee493c22929b2585ea8a8a
Adarsh <adarshgupta2468@gmail.com>
2022-05-22 12:00:53 +0530

    socket io + client🔥

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+    console.log("first")
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.htm');
+  });
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
